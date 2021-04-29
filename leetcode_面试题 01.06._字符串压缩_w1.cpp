d1d188fa1e1f16f4748c7b7cdfaf0d060f404c2a
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-04-29 15:25:01 +0800

    Add files via upload

---
+++ leetcode_面试题 01.06._字符串压缩_w1.cpp
@@ -0,0 +1,83 @@
+/* ------------------------------------------------------------------|
+字符串压缩。利用字符重复出现的次数，编写一种方法，实现基本的字符串压缩功能。比如，字符串aabcccccaaa会变为a2b1c5a3。若“压缩”后的字符串没有变短，则返回原先的字符串。你可以假设字符串中只包含大小写英文字母（a至z）。
+
+示例1:
+
+ 输入："aabcccccaaa"
+ 输出："a2b1c5a3"
+示例2:
+
+ 输入："abbccd"
+ 输出："abbccd"
+ 解释："abbccd"压缩后为"a1b2c2d1"，比原字符串长度更长。
+提示：
+
+字符串长度在[0, 50000]范围内。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/compress-string-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*
+*
+*	执行用时：8 ms, 在所有 C++ 提交中击败了90.41%的用户
+*	内存消耗：7.2 MB, 在所有 C++ 提交中击败了82.18%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+string compressString(string S) {
+    string res;
+    int tmp = 0;
+    char curr = S[0];
+    for (int i = 0; i <= S.size(); ++i) {
+        if (i == S.size() || S[i] != curr) {
+            res += curr + to_string(tmp);
+            tmp = 1;
+            curr = S[i];
+        }
+        else {
+            tmp++;
+        }
+    }
+    return res.size() >= S.size() ? S : res;
+}
